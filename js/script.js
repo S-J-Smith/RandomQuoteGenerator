9bc1c3e74b1f81025a3d22fec2f0ae5c840914f9
Steve Smith <58996112+S-J-Smith@users.noreply.github.com>
2020-10-19 17:37:13 +0100

    Comments

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ project 1 - A Random Quote Generator
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
- * `quotes` array 
+ * `quotes` array - please note there is i have added the properties for two of the citation's but not the rest as this was hard to find.
 ***/
 var quotes = [
   {
@@ -43,34 +43,28 @@ var quotes = [
   }
 ];
 
-//console.log(quotes);
-//console.table(quotes);
-//console.dir(quotes);
+/*console.log(quotes); - this is to to log the quotes array to the console.*/
+/*console.table(quotes); - this is to log the quotes array to the console in table format */
+//console.dir(quotes); - this is to see the quote array in a directory style format. */ 
 
 /***
- * `getRandomQuote` function
+ * `getRandomQuote` function - generates random number and returns its index number from the array.
 ***/
 function getRandomQuote(array) {
 
-  var quoteIndex = Math.floor( Math.random() * quotes.length );
+  var quoteIndex = Math.floor( Math.random() * quotes.length ); // generates a random index number from the Object array and assigns to the quoteIndex variable.
 
-  /*
-  for (var i= 0; i < array.length; i++) {
-    var randomQuote = array[quoteIndex];
-  }
-  */
-
-  return array[quoteIndex];
+  return array[quoteIndex]; //returns the index number and quote from the array.
 
 }
 
-var result = getRandomQuote(quotes);
+var result = getRandomQuote(quotes); //returns a random quote from the array by calling the qyote from the array and getRandomQuote function.
 
-//console.log(result);
+//console.log(result); - logs out the above to the console.
 
 
 /***
- * `printQuote` function
+ * `printQuote` function - The app should display a new quote each time the user clicks the "Show another quote" button using a printQuote function.
 ***/
 
 function printQuote() {
@@ -96,4 +90,4 @@ function printQuote() {
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
 
-//console.log( printQuote() );
+//console.log( printQuote() ); - logs the printQuote function to the console.
